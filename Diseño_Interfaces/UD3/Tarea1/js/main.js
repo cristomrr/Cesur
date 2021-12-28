25c0d0128f64cd1c61a20c63839124639907e4cb
Cristo Manuel Rodríguez Rodríguez <78050608+cristomrr@users.noreply.github.com>
2021-12-28 22:59:36 +0000

    Mejoras de IU

--- Diseño_Interfaces/UD3/Tarea1/js/main.js
+++ Diseño_Interfaces/UD3/Tarea1/js/main.js
@@ -1,20 +1,20 @@
-(()=> {
+(() => {
     // Añadimos Texto animado al header
     const textoAnimado = document.querySelectorAll('.info-animado');
     const textoInfo = [
-        ["Uno de los mejores Grupos de todos los tiempos", "Cristo M."],
+        ["Uno de los mejores Grupos de todos los tiempos", "Cristo M. R.R."],
         ["Un verdadero espectáculo que no debes perder", "Javier Mechado"],
         ["Descubrimiento de la verdadera música", "Jesús Apache"],
         ["La verdadera melodía del paraíso", "Matilde socrates"],
-    ]
+    ];
 
     let index = 0;
     let len = textoInfo.length;
-    setInterval(()=>{
-            textoAnimado[0].innerHTML = textoInfo[index][0];
-            textoAnimado[1].innerHTML = textoInfo[index][1];
-        index = (index >= len-1) ? index = 0 : index += 1;
-    },8000);
+    setInterval(() => {
+        textoAnimado[0].innerHTML = textoInfo[index][0];
+        textoAnimado[1].innerHTML = textoInfo[index][1];
+        index = (index >= len - 1) ? index = 0 : index += 1;
+    }, 8000);
 
     // Añadimos vídeos de los inputs
     const videoYoutube = document.querySelector('.seccion-youtube > iframe');
@@ -25,11 +25,32 @@
         "https://www.youtube.com/embed/_bP6aVG6L1w",
         "https://www.youtube.com/embed/JGftIcp2SC0",
         "https://www.youtube.com/embed/c4UGLm2QjC8",
-    ]
+    ];
 
     for (let i = 0, lenVideos = enlacesVideos.length; i < lenVideos; i++) {
-        musicasAlbum[i].addEventListener('click', ()=>{
+        musicasAlbum[i].addEventListener('click', () => {
             videoYoutube.src = enlacesVideos[i];
+        });
+    }
+
+    // Añadimos info del cantante al pulsar
+    const infoCantante = document.querySelector('.info-cantante');
+    const nombreCantante = document.querySelector('.nombre-cantante');
+    const cantantes = document.querySelectorAll('#seccion-cantantes > img');
+    const nombresCantantes = [
+        "Brian Johnson",
+        "Dave Evans",
+        "Malcolm Young",
+        "PhilRudd",
+        "AngusYoung",
+        "CliffWilliams",
+    ];
+
+    for (let i = 0, lenCantantes = cantantes.length; i < lenCantantes; i++) {
+        cantantes[i].addEventListener('click', (e) => {
+            if (infoCantante.classList.contains('info-oculto'))
+                infoCantante.classList.toggle('info-oculto');
+            nombreCantante.innerHTML = nombresCantantes[i];
         });
     }
 })();
